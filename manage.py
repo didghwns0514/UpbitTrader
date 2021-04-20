143735794d52490b47241ddee1e04bccf63410c5
Hhhh Jjjj <didghwns0514@gmail.com>
2021-04-20 16:52:46 +0900

    fixing manage.py...

--- manage.py
+++ manage.py
@@ -8,8 +8,8 @@ env_string = 'config.settings.deploy' \
     if not bool(os.getenv('IS_DEVELOP', True)) \
     else 'config.settings.development'
 print(f'env_string : {env_string}')
-print(f'bool(os.environ.get("IS_DEVELOP", True)) : ', bool(os.getenv('IS_DEVELOP', True)))
-print(f"os.environ.get('IS_DEVELOP') : {os.getenv('IS_DEVELOP')}")
+print(f'bool(os.getenv("IS_DEVELOP", True)) : ', bool(os.getenv('IS_DEVELOP', True)))
+print(f"os.getenv('IS_DEVELOP') : {os.getenv('IS_DEVELOP')}")
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', env_string)

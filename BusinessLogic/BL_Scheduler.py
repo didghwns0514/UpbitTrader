8b5c910d72a89156ef850b11acbe795888319e27
Yang <uib58518@contiwan.com>
2021-04-21 17:32:45 +0900

    Api request returns must be dealt with asyncio...

--- BusinessLogic/BL_Scheduler.py
+++ BusinessLogic/BL_Scheduler.py
@@ -1,7 +1,9 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from .UpbitInterface import UpbitAPI
 
-
+"""
+https://tomining.tistory.com/138
+"""
 def start_upbit():
     scheduler = BackgroundScheduler()
     scheduler.add_job(UpbitAPI.request_tick_data, 'interval', seconds=1)
--- BusinessLogic/UpbitInterface.py
+++ BusinessLogic/UpbitInterface.py
@@ -5,11 +5,17 @@ from threading import Thread
 import datetime
 import time
 
+"""
+https://wikidocs.net/31063
+https://wikidocs.net/117438
+
+"""
 
 class UpbitAPI:
 
-    def __init__(self): pass
+    def __init__(self):
+        var_check_class = 'Upbit-api'
 
     @staticmethod
     def request_tick_data():
-        print(f'in upbit API : ', datetime.datetime.now())
+        print(f'in upbit API : {UpbitAPI.var_check_class}', datetime.datetime.now())

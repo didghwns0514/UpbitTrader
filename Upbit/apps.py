582d14e99d77f218eb9b7da648db73a0746a601a
Hhhh Jjjj <didghwns0514@gmail.com>
2021-05-07 23:08:47 +0900

    fixing index

--- Upbit/apps.py
+++ Upbit/apps.py
@@ -1,14 +1,5 @@
 from django.apps import AppConfig
 
-# class TaskThread(Thread):
-#
-#     def __init__(self):
-#         Thread.__init__(self)
-#
-#     # override run method
-#     def run(self):
-#         print('Thread running : ', datetime.datetime.now())
-#         #time.sleep(1)
 
 class UpbitConfig(AppConfig):
     print(f'UpbitConfig is up!')
